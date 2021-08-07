391b2033db79856bf31a11f5716f94c909cf9b96
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-08-07 09:55:01 +0530

    router file changed

--- backend/routes.js
+++ backend/routes.js
@@ -3,6 +3,5 @@ const authController = require('./controllers/auth-controller');
 
 router.post('/api/send-otp', authController.sendOtp);
 router.post('/api/verify-otp', authController.verifyOtp);
-
 module.exports = router;
 
